1ff1dc60939d58ddbe1158e95042d077432ee19c
helloxyz1 <darshil.db@gmail.com>
2022-06-27 10:43:15 +0530

    Update streamlit_app.py
    numpy intro

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,17 +1,9 @@
-import streamlit
-
-# Draw a title and some text to the app:
-'''
-# This is the document title
-
-This is some _markdown_.
-'''
-
+import streamlit as st
+import numpy as np
 import pandas as pd
 
-df = pd.DataFrame({
-  'first column': [1, 2, 3, 4],
-  'second column': [10, 20, 30, 40]
-})
+dataframe = pd.DataFrame(
+    np.random.randn(10, 20),
+    columns=('col %d' % i for i in range(20)))
 
-df
+st.dataframe(dataframe.style.highlight_max(axis=0))
