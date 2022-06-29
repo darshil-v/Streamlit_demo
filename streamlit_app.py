48e9cc37b7958df8d5d958404f7b2eb11d86abd1
helloxyz1 <darshil.db@gmail.com>
2022-06-29 13:15:29 +0530

    Update streamlit_app.py
    try-except block

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,11 +28,13 @@ try:
   fruit_choice = streamlit.text_input('What fruit would you like information about?')
   if not fruit_choice:
     streamlit.error('Please enter valid fruit choice.')
-else:
-  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-  fruitvice_normalised = pd.json_normalize(fruityvice_response.json())
-  streamlit.dataframe(fruitvice_normalised)
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruitvice_normalised = pd.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruitvice_normalised)
 
+except URLError as e:
+  streamlit.error()
   
 streamlit.stop()
 
