2e1b1feaf926167b694857658c530e19898691cb
helloxyz1 <darshil.db@gmail.com>
2022-06-29 12:31:07 +0530

    Update streamlit_app.py
    added snowflake connection

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit #as st
 import numpy as np
 import pandas as pd
-import snowflake.connector
+
 
 streamlit.header('Breakfast Menu')
 streamlit.text('Omega 3 & Blueberry Oatmeal')
@@ -25,9 +25,19 @@ fruit_choice = streamlit.text_input('What fruit would you like information about
 streamlit.write('The user entered ', fruit_choice)
 
 import requests
+
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
 #streamlit.text(fruityvice_response.json())
 
 #beautify json response
 fruitvice_normalised = pd.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruitvice_normalised)
+
+import snowflake.connector
+
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+my_cur = my_cnx.cursor()
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+my_data_row = my_cur.fetchone()
+streamlit.text("Hello from Snowflake:")
+streamlit.text(my_data_row)
